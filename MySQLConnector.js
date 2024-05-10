81da5652051239fe798abe8fa5f56a29a1785cb4
Kirui Langat <ted.kirui@students.ku.ac.ke>
2024-05-10 12:35:33 +0300

    privacy

--- MySQLConnector.js
+++ MySQLConnector.js
@@ -1,10 +1,13 @@
 import { createConnection } from 'mysql';
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const db_config = {
-  host: 'housedesigns.co.ke',
-  user: 'housedes_HDKAdmin',
-  password: 'HDK@2024',
-  database: 'housedes_HDK2024'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 };
 
 let connection;
